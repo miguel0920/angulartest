a68dbd96dc46606c18fa514af456ab9d92e6f993
Miguel Angel Barahona <miguelbarahona00@gmail.com>
2025-08-06 14:58:35 -0500

    add new message

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { DatePipe, formatDate } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -6,8 +7,8 @@ import { RouterOutlet } from '@angular/router';
   standalone: true,
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'angulartest';
+  title = `Despliegue Angular en Cloud Panel`;
 }
