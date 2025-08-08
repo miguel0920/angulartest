900612d557f7824b976f5877162c517a9c322a48
Miguel Angel Barahona <miguelbarahona00@gmail.com>
2025-08-08 15:25:07 -0500

    test request  login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
-import { DatePipe, formatDate } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -11,4 +11,18 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = `Despliegue Angular en Cloud Panel`;
+  url : string = 'http://localhost:6500/security/token/'
+  tokenTest: any = { token: '' };
+  constructor(private _httpClient: HttpClient) {
+    _httpClient
+      .post(this.url, {
+        email: 'admin@shiffer.com',
+        password: 'Admin581@',
+      })
+      .subscribe(
+        (data  => {
+          this.tokenTest = data;
+        })
+      );
+  }
 }
